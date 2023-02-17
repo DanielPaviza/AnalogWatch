6364b61d3ca5da3e3153798391ba85009ddc7076
Daniel Pavíza <pavizek@gmail.com>
2023-02-17 10:59:57 +0100

    Přidání funkčnosti ručiček hodin

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,11 +29,52 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.tbConsole = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 50);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(360, 299);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // tbConsole
+            // 
+            this.tbConsole.Location = new System.Drawing.Point(44, 12);
+            this.tbConsole.Name = "tbConsole";
+            this.tbConsole.Size = new System.Drawing.Size(257, 23);
+            this.tbConsole.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(384, 361);
+            this.Controls.Add(this.tbConsole);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private TextBox tbConsole;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,72 @@
 namespace AnalogWatch
 {
+    using System.Drawing;
+    using System.Drawing.Drawing2D;
+
     public partial class Form1 : Form
     {
+
+        private DateTime time = DateTime.Now;
+        private int degreesEachSecond = 6;
+        private int minLineLength, secLineLength;
+        private Point center;
+        private Graphics g;
+        private Pen secPen = new Pen(Color.Red, 2);
+        private Pen minPen = new Pen(Color.Black, 3);
+
+        public object Calcs { get; private set; }
+
         public Form1()
         {
             InitializeComponent();
+
+            center = new Point(pictureBox1.Width / 2, pictureBox1.Height / 2);
+            minLineLength = pictureBox1.Height / 2 - 20;
+            secLineLength = pictureBox1.Height/2 - 10;
+            timer1.Start();
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+            DrawMinutes(e.Graphics, minPen);
+            DrawSeconds(e.Graphics, secPen);
+        }
+
+        private void DrawMinutes(Graphics g, Pen p)
+        {
+            int min = time.Minute;
+            g.DrawLine(p, center, GetEndPoint(min, minLineLength));
+        }
+
+        private void DrawSeconds(Graphics g, Pen p)
+        {
+
+            int sec = time.Second;
+            g.DrawLine(p, center, GetEndPoint(sec, secLineLength));
+
+        }
+
+        private Point GetEndPoint(int num, int lineLength)
+        {
+            double angle = ToRadians(-1 * (num * degreesEachSecond));
+            int endX = Convert.ToInt32(Math.Round(center.X - lineLength * Math.Sin(angle)));
+            int endY = Convert.ToInt32(Math.Round(center.Y - lineLength * Math.Cos(angle)));
+
+            return new Point(endX, endY);
+        }
+
+
+        private double ToRadians(double x) {
+            return (Math.PI / 180) * x;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            time = DateTime.Now;
+            tbConsole.Text = time.ToString();
+
+            pictureBox1.Refresh();
         }
     }
 }
