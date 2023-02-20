d5e9c3151c5c14a42a79b76274626ce9cf42ae9a
Daniel Pavíza <pavizek@gmail.com>
2023-02-20 13:55:40 +0100

    Dokončený widget hodiny

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,43 +29,80 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.buttonClose = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = global::AnalogWatch.Properties.Resources.clock;
-            this.pictureBox1.Location = new System.Drawing.Point(4, 3);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(360, 360);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
-            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
             // 
             // timer1
             // 
             this.timer1.Interval = 1000;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.SizeAll;
+            this.pictureBox1.ErrorImage = null;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(6, 6);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(354, 354);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonClose.BackColor = System.Drawing.Color.Transparent;
+            this.buttonClose.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.buttonClose.Location = new System.Drawing.Point(327, 7);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(32, 28);
+            this.buttonClose.TabIndex = 0;
+            this.buttonClose.Text = " X";
+            this.buttonClose.UseVisualStyleBackColor = false;
+            this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.ButtonFace;
-            this.ClientSize = new System.Drawing.Size(366, 365);
+            this.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.ClientSize = new System.Drawing.Size(366, 366);
+            this.Controls.Add(this.buttonClose);
             this.Controls.Add(this.pictureBox1);
+            this.Cursor = System.Windows.Forms.Cursors.SizeNWSE;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Opacity = 0.92D;
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Hodiny";
+            this.TransparencyKey = System.Drawing.Color.Transparent;
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private PictureBox pictureBox1;
         private System.Windows.Forms.Timer timer1;
+        private PictureBox pictureBox1;
+        private Button button1;
+        private Button buttonClose;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -9,22 +9,39 @@ namespace AnalogWatch
         private DateTime time = DateTime.Now;
         private int degreesEachSecond = 6;
         private int hLineLength, minLineLength, secLineLength;
-        private Point center;
+        private Point centerPicture;
         private Graphics g;
         private Pen hPen = new Pen(Color.Black, 5);
         private Pen minPen = new Pen(Color.Black, 3);
         private Pen secPen = new Pen(Color.Red, 2);
+        private bool draggingForm = false;
+        private bool draggingPicture = false;
+        private int pictureMargin = 6;
+        private int minSize = 130;
+        private int maxSize = 600;
+        private Point lastLocation;
 
         public Form1()
         {
             InitializeComponent();
 
-            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
-            center = new Point(pictureBox1.Width / 2, pictureBox1.Height / 2);
-            hLineLength = pictureBox1.Height / 2 - 60;
-            minLineLength = pictureBox1.Height / 2 - 40;
-            secLineLength = pictureBox1.Height/2 - 30;
+            buttonClose.FlatStyle = FlatStyle.Flat;
+            buttonClose.FlatAppearance.BorderSize = 0;
+            ScalePicture();
             timer1.Start();
+        }
+
+        private void ScalePicture() {
+
+            pictureBox1.Location = new Point(pictureMargin, pictureMargin);
+            pictureBox1.Width = this.Width - pictureMargin * 2;
+            pictureBox1.Height = this.Height - pictureMargin * 2;
+            centerPicture = new Point(pictureBox1.Width / 2, pictureBox1.Height / 2);
+
+            int fullSize = pictureBox1.Height / 2;
+            hLineLength = fullSize / 2;
+            minLineLength = hLineLength + fullSize/4;
+            secLineLength = minLineLength + fullSize / 10;
         }
 
         private void pictureBox1_Paint(object sender, PaintEventArgs e)
@@ -33,37 +50,119 @@ namespace AnalogWatch
             DrawHours(e.Graphics, hPen);
             DrawMinutes(e.Graphics, minPen);
             DrawSeconds(e.Graphics, secPen);
+
+            Color borderColor = Color.FromArgb(220, 222, 220);
+            ControlPaint.DrawBorder(e.Graphics, pictureBox1.ClientRectangle, borderColor, ButtonBorderStyle.Solid);
         }
 
         private void DrawHours(Graphics g, Pen p)
         {
             int h = time.Hour;
-            g.DrawLine(p, center, GetEndPoint(h, hLineLength));
+            g.DrawLine(p, centerPicture, GetEndPoint(h, hLineLength));
         }
 
         private void DrawMinutes(Graphics g, Pen p)
         {
             int min = time.Minute;
-            g.DrawLine(p, center, GetEndPoint(min, minLineLength));
+            g.DrawLine(p, centerPicture, GetEndPoint(min, minLineLength));
         }
 
         private void DrawSeconds(Graphics g, Pen p)
         {
 
             int sec = time.Second;
-            g.DrawLine(p, center, GetEndPoint(sec, secLineLength));
+            g.DrawLine(p, centerPicture, GetEndPoint(sec, secLineLength));
 
         }
 
         private Point GetEndPoint(int num, int lineLength)
         {
             double angle = ToRadians(-1 * (num * degreesEachSecond));
-            int endX = Convert.ToInt32(Math.Round(center.X - lineLength * Math.Sin(angle)));
-            int endY = Convert.ToInt32(Math.Round(center.Y - lineLength * Math.Cos(angle)));
+            int endX = Convert.ToInt32(Math.Round(centerPicture.X - lineLength * Math.Sin(angle)));
+            int endY = Convert.ToInt32(Math.Round(centerPicture.Y - lineLength * Math.Cos(angle)));
 
             return new Point(endX, endY);
         }
 
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                draggingForm = true;
+            }
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (draggingForm)
+            {
+
+                bool error = false;
+                if(this.Width < minSize)
+                {
+                    this.Width = minSize;
+                    this.Height = minSize;
+                    error = true;
+                }
+
+                if (this.Width > maxSize) {
+
+                    this.Width = maxSize;
+                    this.Height = maxSize;
+                    error = true;
+                }
+
+                if (error) {
+                    Cursor.Position = new Point(this.Location.X + this.Width, this.Location.Y + this.Height);
+                    return;
+                }
+
+                Point mouseLocation = new Point(e.X, e.Y);
+                this.Width = this.Location.X + (mouseLocation.X - this.Location.X);
+                this.Height = this.Width;
+                ScalePicture();
+            }
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                draggingForm = false;
+            }
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                lastLocation = e.Location;
+                draggingPicture = true;
+            }
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (draggingPicture)
+            {
+                int dx = e.Location.X - lastLocation.X;
+                int dy = e.Location.Y - lastLocation.Y;
+                this.Location = new Point(this.Location.X + dx, this.Location.Y + dy);
+            }
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                draggingPicture = false;
+            }
+        }
+
+        private void buttonClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
 
         private double ToRadians(double x) {
             return (Math.PI / 180) * x;
